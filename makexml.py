caa302e2f17e4e9019d383e46eaceccafa3f6d9a
voledesoto <50086469+voledesoto@users.noreply.github.com>
2019-06-23 23:21:24 +0100

    Update makexml.py

--- makexml.py
+++ makexml.py
@@ -2,6 +2,8 @@ from random import randint
 
 fifthsdict = {"Cb": -7, "Gb": -6, "Db": -5, "Ab": -4, "Eb": -3, "Bb": -2, "F": -1, "C": 0, "G": 1, "D": 2, "A": 3, "E": 4, "B": 5, "F#": 6, "C#": 7,}
 
+print(fifthsdict[0])
+
 header = '<?xml version="1.0" encoding="UTF-8" standalone="no"?>\n<!DOCTYPE score-partwise PUBLIC\n"-//Recordare//DTD MusicXML 3.1 Partwise//EN"\n"http://www.musicxml.org/dtds/partwise.dtd">\n<score-partwise version="3.1">\n<part-list>\n<score-part id="P1">\n<part-name>Music</part-name>\n</score-part>\n</part-list>\n<part id="P1">\n'
 
 
@@ -53,10 +55,9 @@ def setupstart():
 
 print(header + "\n" + setupstart())
 
-#notesxml[notesarray[random]]
 
-notesarray = ["Af", "A", "As", "Bf", "B", "Bs", "Cf", "C", "Cs", "Df", "D", "Ds", "Ef", "E", "Es", "Ff", "F", "Fs", "Gf", "G", "Gs"]
-natnotesarray = ["A", "B", "C", "D", "E", "F", "G"]
+# NEEDED? notesarray = ["Af", "A", "As", "Bf", "B", "Bs", "Cf", "C", "Cs", "Df", "D", "Ds", "Ef", "E", "Es", "Ff", "F", "Fs", "Gf", "G", "Gs"]
+
 notesxml = {
   "Af": "<step>A</step>"+'\n'+"<accidental>flat</accidental>",
   "A": "<step>A</step>",
@@ -79,24 +80,9 @@ notesxml = {
   "Gf": "<step>G</step>"+'\n'+"<accidental>flat</accidental>",
   "G": "<step>G</step>",
   "Gs": "<step>G</step>"+'\n'+"<accidental>sharp</accidental>"}
-natnotesxml = {
-    "A": "<step>A</step>",
-    "B": "<step>B</step>",
-    "C": "<step>C</step>",
-    "D": "<step>D</step>",
-    "E": "<step>E</step>",
-    "F": "<step>F</step>",
-    "G": "<step>G</step>",
- }
-accidentalsarrays = {"f": "<accidental>flat</accidental>", "s": "<accidental>sharp</accidental>", "ff": "<accidental>double-flat</accidental>", "ss": "<accidental>double-sharp</accidental>"}
-
-keypositions = notesarray.index(starting_note_key)
-
-#add the notes as strings that you want in the arrays for each key
-keysdict = { "Af": ["Af", "C"], "A": [], "As": [], "Bf": [], "B": [], "Bs": [], "Cf": [], "C": [], "Cs": [], "Df": [], "D": [], "Ds": [], "Ef": [], "E": [], "Es": [], "Ff": [], "F": [], "Fs": [], "Gf": [], "G": [], "Gs": []}
-
 
 lengtharrary = ["Sb", "M", "C", "Q", "Sq", "Dq"]
+
 #i guessed these duration and type values
 lengthxml = {
   "Sb": "<duration>4</duration>"+'\n'+"    <type>whole</type>",
@@ -108,59 +94,20 @@ lengthxml = {
 }
 
 lengthvals = {"Sb": 1, "M": 1/2, "C": 1/4, "Q": 1/8, "Sq": 1/16, "Dq": 1/32}
-leftlengthvals = [float(1), float(1/2), float(1/4), float(1/8), float(1/16), float(1/32)]
+leftlengthvals = [str(1), str(1/2), str(1/4), str(1/8), str(1/16), str(1/32)]
 
 
-def make_note(notenum, lengthnum):
+def make_note(step, length):
     note_string = """  <note>
     <pitch>
-      """+str(natnotesxml[natnotesarray[notenum]])+"""
-      <octave>"""+str(octaveposition)+"""</octave>
+      <step> """ + str() + """
+      <octave>""" + str() + """</octave>
     </pitch>
-    """+lengthxml[lengtharrary[lengthnum]]+"""
+    """ + str() + """
   </note>"""
     return note_string
 
 testnote = make_note(2, 2)
-#print(testnote)
-
-
-def first_bar(beatss):
-    bar_string = ""
-    beatsleft = beatss
-    while beatsleft > 0:
-        notenum = randint(0,6)
-        lengthnum = randint(0,5)
-        if (leftlengthvals[lengthnum]-beatsleft) >= 0:
-            bar_string += make_note(notenum, lengthnum)
-            timeprogress = lengthvals[lengtharrary[lengthnum]]
-            beatsleft -= timeprogress
-        else:
-            continue
-    bar_string += "\n</measure>"
-    global measurenumber
-    measurenumber = 2
-    return bar_string
-
-testfirstbar = first_bar(TotalBeats)
-print(testfirstbar)
-
-def make_bar(beatss):
-    bar_string = "<measure number = "+"\""+str(measurenumber)+"\""+">"
-    beatsleft = beatss
-    while beatsleft > 0:
-        notenum = randint(0,6)
-        lengthnum = randint(0,5)
-        if (leftlengthvals[lengthnum]-beatsleft) >= 0:
-            bar_string += make_note(notenum, lengthnum)
-            timeprogress = lengthvals[lengtharrary[lengthnum]]
-            beatsleft -= timeprogress
-        else:
-            continue
-    bar_string += "\n</measure>"
-    global measurenumber
-    measurenumber += 1
-    return bar_string
 
 
 
@@ -172,7 +119,7 @@ def addtuple(actual, normal):
   return tuplestring
 
 
-#so far only just printing strings that will be written to file
+#so far only just prstring strings that will be written to file
 
 #f = open("output.xml", "a")
 #f.write(contents)
